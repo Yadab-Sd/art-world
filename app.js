c4c0168a0925bfd26ce5e9da848044e08181b7f3
Yadab <yadab.sd2013@gmail.com>
2023-05-07 21:16:22 -0500

    feat(static): static file serving done

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const express = require("express");
-const app = express();
-const data = require("./utils/data.json");
-
 require("dotenv").config();
+const express = require("express");
+const data = require("./utils/data.json");
+const path = require("path");
 
+const app = express();
 
-const server = app.listen(process.env.PORT, function () {
-  console.log(data.serverMessage.startListening, server.address().port);
+app.set(process.env.SERVER_PORT_KEYNAME, process.env.PORT);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  console.log(req.url, req.method);
+  next();
 });
+
+app.use(express.static(path.join(__dirname, process.env.STATIC_FOLDER_NAME)));
+
+const server = app.listen(
+  app.get(process.env.SERVER_PORT_KEYNAME),
+  function () {
+    console.log(data.serverMessage.startListening, server.address().port);
+  }
+);
